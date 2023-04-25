10b616614e127031b9bf0bb54450892a4612fc5f
Blonxx <Bartoszliberda@gmail.com>
2023-04-25 17:46:22 +0100

    aws fix ?

--- medusa-config.js
+++ medusa-config.js
@@ -65,8 +65,8 @@ const plugins = [
         s3_url: process.env.S3_URL,
         bucket: process.env.S3_BUCKET,
         region: process.env.S3_REGION,
-        access_key_id: process.env.S3_ACCESS_KEY_ID,
-        secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+        access_key_id: process.env.AWS_ACCESS_KEY_ID,
+        secret_access_key: process.env.AWS_SECRET_ACCESS_KEY,
     },
   },
 ];
